0691f4fd471cedac501d9d46535f2ec2aacea8cb
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-09-06 19:11:21 +0300

    Added a logging level environment variable logic.

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -1,26 +1,47 @@
 package config
 
-import "os"
+import (
+	"os"
+	"log/slog"
+)
 
 type Config struct {
-    Port         string
-    InstanceName string
+	Port				string
+	InstanceName	string
+	LogLevel		slog.Level
+}
+
+func getLogLevel(level string) slog.Level {
+	switch level {
+	case "DEBUG":
+		return slog.LevelDebug
+	case "INFO":
+		return slog.LevelInfo
+	case "WARN":
+		return slog.LevelWarn
+	case "ERROR":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
 }
 
 func Load() *Config {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    instanceName := os.Getenv("INSTANCE_NAME")
-    if instanceName == "" {
-        instanceName = "myapp-1"
-    }
+	instanceName := os.Getenv("INSTANCE_NAME")
+	if instanceName == "" {
+		instanceName = "myapp-1"
+	}
 
-    return &Config{
-        Port:         port,
-        InstanceName: instanceName,
-    }
+	logLevel := getLogLevel(os.Getenv("LOG_LEVEL"))
+
+	return &Config{
+		Port:				port,
+		InstanceName:	instanceName,
+		LogLevel:		logLevel,
+	}
 }
-
