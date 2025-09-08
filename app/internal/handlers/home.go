f12da7a5e858df2c9cff02778e4157c108666f0e
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-09-08 21:02:38 +0300

    Working languages for all components but not for the navbar and footer

--- app/internal/handlers/home.go
+++ app/internal/handlers/home.go
@@ -1,17 +1,18 @@
 package handlers
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/a-h/templ"
-    "NTG/internal/middleware"
-    "NTG/web/templates/pages/home"
+	"github.com/a-h/templ"
+	"NTG/internal/middleware"
+	"NTG/web/templates/pages/home"
 )
 
 func Home() http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        phrases := r.Context().Value(middleware.PhrasesKey).(map[string]map[string]string)
-        
-        templ.Handler(home.Handler(phrases)).ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		language := r.Context().Value(middleware.LanguageKey).(string)
+		phrases := r.Context().Value(middleware.PhrasesKey).(map[string]map[string]string)
+		
+		templ.Handler(home.Handler(language, phrases)).ServeHTTP(w, r)
+	})
 }
