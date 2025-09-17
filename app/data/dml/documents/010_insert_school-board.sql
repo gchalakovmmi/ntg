2bf9dc9b24dfa0003c330a6b22fd1af0c410c3b7
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-09-17 23:19:21 +0300

    Inserted the documents for the school_board section and added the phrases for them.

---
+++ app/data/dml/documents/010_insert_school-board.sql
@@ -0,0 +1,14 @@
+-- School Board section phrases
+INSERT INTO phrases (language, page, section, key, phrase) VALUES
+('bg', 'documents', 'school_board', 'title', 'Училищен съвет'),
+('en', 'documents', 'school_board', 'title', 'School Board');
+-- School Board documents display names
+INSERT INTO phrases (language, page, section, key, phrase) VALUES
+('bg', 'documents', 'school_board', 'doklad_deynost', 'Доклад за дейността на УН към НТГ - Пловдив за 2012-2013 учебна година'),
+('en', 'documents', 'school_board', 'doklad_deynost', 'Activity Report of the School Board at NTS - Plovdiv for the 2012-2013 academic year'),
+('bg', 'documents', 'school_board', 'byudjet', 'Бюджет на УН към НТГ - Пловдив за 2013-2014 учебна година'),
+('en', 'documents', 'school_board', 'byudjet', 'Budget of the School Board at NTS - Plovdiv for the 2013-2014 academic year'),
+('bg', 'documents', 'school_board', 'pokana_os_19.06.2025', 'Покана за ОС на УН за 19.06.2025 г.'),
+('en', 'documents', 'school_board', 'pokana_os_19.06.2025', 'Invitation for School Board Meeting on June 19, 2025'),
+('bg', 'documents', 'school_board', 'ustav', 'Устав на УН към НТГ - Пловдив'),
+('en', 'documents', 'school_board', 'ustav', 'Statute of the School Board at NTS - Plovdiv');
