9c1811ab99d23f7426d03eabc16ded3169d1bd3a
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-10-08 16:33:21 +0300

    Added the text for the specialties document and section.

---
+++ app/data/dml/documents/017_specialties.sql
@@ -0,0 +1,9 @@
+-- Document templates section phrases
+INSERT INTO phrases (language, page, section, key, phrase) VALUES
+('bg', 'documents', 'specialties', 'title', 'Специалности'),
+('en', 'documents', 'specialties', 'title', 'Specialties');
+
+-- Scholarships documents display names
+INSERT INTO phrases (language, page, section, key, phrase) VALUES
+('bg', 'documents', 'specialties', '1690175 NTG Dni na profesiite 21-22.04.2023', 'Дни на професиите'),
+('en', 'documents', 'specialties', '1690175 NTG Dni na profesiite 21-22.04.2023', 'Days of the professions');
