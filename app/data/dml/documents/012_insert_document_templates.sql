ec0710056e0125eec008e73c91f56b2d42d6f758
tazera <lyubomir.tsvetkov141@gmail.com>
2025-10-11 20:30:46 +0300

    Changed the documents and updated programs&projects + fix the routes of the documents

--- app/data/dml/documents/012_insert_document_templates.sql
+++ app/data/dml/documents/012_insert_document_templates.sql
@@ -1,15 +1,15 @@
 -- Document templates section phrases
 INSERT INTO phrases (language, page, section, key, phrase) VALUES
-('bg', 'base', 'dzi', 'title', 'Документи за ученика'),
-('en', 'base', 'dzi', 'title', 'Documents for the Student'),
-('de', 'base', 'dzi', 'title', 'Dokumente für den Schüler'),
-('fr', 'base', 'dzi', 'title', 'Documents pour l''élève'),
-('es', 'base', 'dzi', 'title', 'Documentos para el Estudiante');
+('bg', 'base', 'document_templates', 'title', 'Документи за ученика'),
+('en', 'base', 'document_templates', 'title', 'Documents for the Student'),
+('de', 'base', 'document_templates', 'title', 'Dokumente für den Schüler'),
+('fr', 'base', 'document_templates', 'title', 'Documents pour l''élève'),
+('es', 'base', 'document_templates', 'title', 'Documentos para el Estudiante');
 
 -- Scholarships documents display names
 INSERT INTO phrases (language, page, section, key, phrase) VALUES
-('bg', 'base', 'dzi', 'zap2470_dzi_30082024', 'Бланка за отсъствия за учебната 2025/2026 година'),
-('en', 'base', 'dzi', 'zap2470_dzi_30082024', 'Absence Form for the 2025/2026 School Year'),
-('de', 'base', 'dzi', 'zap2470_dzi_30082024', 'Entschuldigungsformular für das Schuljahr 2025/2026'),
-('fr', 'base', 'dzi', 'zap2470_dzi_30082024', 'Formulaire d''absence pour l''année scolaire 2025/2026'),
-('es', 'base', 'dzi', 'zap2470_dzi_30082024', 'Formulario de Ausencia para el Año Escolar 2025/2026');
+('bg', 'base', 'document_templates', 'zap2470_dzi_30082024', 'Бланка за отсъствия за учебната 2025/2026 година'),
+('en', 'base', 'document_templates', 'zap2470_dzi_30082024', 'Absence Form for the 2025/2026 School Year'),
+('de', 'base', 'document_templates', 'zap2470_dzi_30082024', 'Entschuldigungsformular für das Schuljahr 2025/2026'),
+('fr', 'base', 'document_templates', 'zap2470_dzi_30082024', 'Formulaire d''absence pour l''année scolaire 2025/2026'),
+('es', 'base', 'document_templates', 'zap2470_dzi_30082024', 'Formulario de Ausencia para el Año Escolar 2025/2026');
--- app/data/dml/documents/013_dzi_insert.sql
+++ app/data/dml/documents/013_dzi_insert.sql
@@ -1,15 +1,15 @@
 -- Document templates section phrases
 INSERT INTO phrases (language, page, section, key, phrase) VALUES
-('bg', 'base', 'document_templates', 'title', 'ДЗИ'),
-('en', 'base', 'document_templates', 'title', 'DZI'),
-('de', 'base', 'document_templates', 'title', 'DZI'),
-('fr', 'base', 'document_templates', 'title', 'DZI'),
-('es', 'base', 'document_templates', 'title', 'DZI');
+('bg', 'base', 'dzi', 'title', 'ДЗИ'),
+('en', 'base', 'dzi', 'title', 'DZI'),
+('de', 'base', 'dzi', 'title', 'DZI'),
+('fr', 'base', 'dzi', 'title', 'DZI'),
+('es', 'base', 'dzi', 'title', 'DZI');
 
 -- Scholarships documents display names
 INSERT INTO phrases (language, page, section, key, phrase) VALUES
-('bg', 'base', 'document_templates', 'absences-family-reasons', 'Заповед за допускане до ДЗИ 2025'),
-('en', 'base', 'document_templates', 'absences-family-reasons', 'DZI Admission Order 2025'),
-('de', 'base', 'document_templates', 'absences-family-reasons', 'DZI-Zulassungsbescheid 2025'),
-('fr', 'base', 'document_templates', 'absences-family-reasons', 'Ordre d''Admission DZI 2025'),
-('es', 'base', 'document_templates', 'absences-family-reasons', 'Orden de Admisión DZI 2025');
+('bg', 'base', 'dzi', 'absences-family-reasons', 'Заповед за допускане до ДЗИ 2025asddsa'),
+('en', 'base', 'dzi', 'absences-family-reasons', 'DZI Admission Order 2025'),
+('de', 'base', 'dzi', 'absences-family-reasons', 'DZI-Zulassungsbescheid 2025'),
+('fr', 'base', 'dzi', 'absences-family-reasons', 'Ordre d''Admission DZI 2025'),
+('es', 'base', 'dzi', 'absences-family-reasons', 'Orden de Admisión DZI 2025');
