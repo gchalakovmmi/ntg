6016a74c0abf6fbf0118ecee5c34705cc788282c
tazera <lyubomir.tsvetkov141@gmail.com>
2025-10-12 13:45:32 +0300

    team page done waiting

---
+++ app/data/dml/phrases/023_insert_team.sql
@@ -0,0 +1,29 @@
+-- Specs education phrases
+
+-- Title
+INSERT INTO phrases (language, page, section, key, phrase) VALUES
+('bg', 'team', 'header', 'title', 'Нашият екип - НТГ'),
+('en', 'team', 'header', 'title', 'Our Team - NTG'),
+
+-- Carousel
+('bg', 'team', 'hero', 'title', 'Нашият екип в Национална търговска гимназия'),
+('bg', 'team', 'hero', 'subtitle', 'Качествено образование за бъдещите лидери в икономиката и бизнеса'),
+('en', 'team', 'hero', 'title', 'Our Team at NTG'),
+('en', 'team', 'hero', 'subtitle', 'Quality education for future leaders in economics and business'),
+
+
+-- Director
+('bg', 'team', 'director', 'name', 'Директор на НТГ'),
+('bg', 'team', 'director', 'position', 'Тект позиция'),
+('bg', 'team', 'director', 'short_bio', 'Тест описание'),
+('en', 'team', 'director', 'name', 'Director of NTG'),
+('en', 'team', 'director', 'position', 'Test Position'),
+('en', 'team', 'director', 'short_bio', 'Test Description'),
+
+-- Deputy Directors
+('bg', 'team', 'documents', 'title', 'Заместник-директори на НТГ'),
+('bg', 'team', 'documents', 'subtitle', 'Заместник-директори на НТГ'),
+('en', 'team', 'documents', 'title', 'Deputy Directors at NTG'),
+('en', 'team', 'documents', 'subtitle', 'Deputy Directors at NTG');
+
+-- Teachers
